e90e30599bbdbce8fd6373c61224f1afcb53e5da
trananhtho207 <trananhtho207@gmail.com>
2015-03-03 03:16:03 +0000

    add realm

--- src/main/java/com/catb/dao/UserDAO.java
+++ src/main/java/com/catb/dao/UserDAO.java
@@ -2,10 +2,13 @@ package com.catb.dao;
 
 import java.util.List;
 
+import com.catb.model.Permission;
 import com.catb.model.User;
 
 public interface UserDAO {
 	
 	public List<User> getUsers();
 	public void addUser(User user);
+	public User getUserByUsername(String username);
+	public List<Permission> getPermissionsByRole(String roleName);
 }
--- src/main/java/com/catb/dao/impl/UserDAOImpl.java
+++ src/main/java/com/catb/dao/impl/UserDAOImpl.java
@@ -2,11 +2,13 @@ package com.catb.dao.impl;
 
 import java.util.List;
 
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.stereotype.Repository;
 
 import com.catb.dao.UserDAO;
+import com.catb.model.Permission;
 import com.catb.model.User;
 
 @Repository
@@ -30,5 +32,27 @@ public class UserDAOImpl implements UserDAO {
 	public void addUser(User user) {
 		Session session = sessionFactory.getCurrentSession();
 		session.save(user);
+	}
+
+	@SuppressWarnings("unchecked")
+	public User getUserByUsername(String username) {
+		Session session = sessionFactory.getCurrentSession();
+		String select = "FROM User WHERE username = :username";
+		Query query = session.createQuery(select);
+		query.setParameter("username", username);
+		List<User> users = (List<User>) query.list();
+		
+		return users.size() > 0 ? users.get(0) : null;
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<Permission> getPermissionsByRole(String roleName) {
+		Session session = sessionFactory.getCurrentSession();
+		String select = "SELECT p FROM Role AS r INNER JOIN r.permissions p WHERE r.name = :roleName";
+		Query query = session.createQuery(select);
+		query.setParameter("roleName", roleName);
+		List<Permission> permissions = (List<Permission>) query.list();
+		
+		return permissions;
 	}
 }
