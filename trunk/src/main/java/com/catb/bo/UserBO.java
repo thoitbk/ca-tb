55b9f27c3f5708e91e7747385966b562f656a1cc
trananhtho207@gmail.com <trananhtho207@gmail.com@683bf793-15f8-c8c1-9392-6aebd92667f2>
2015-03-03 03:16:03 +0000

    add realm

--- trunk/src/main/java/com/catb/bo/UserBO.java
+++ trunk/src/main/java/com/catb/bo/UserBO.java
@@ -1,12 +1,17 @@
 package com.catb.bo;
 
 import java.util.List;
+import java.util.Set;
 
+import com.catb.model.Role;
 import com.catb.model.User;
 
 public interface UserBO {
 	
 	public List<User> getUsers();
 	public void addUser(User user);
-	
+	public User getUserByUsername(String username);
+	public Set<Role> getRolesForUser(String username);
+	public Set<String> getRoleNamesForUser(String username);
+	public Set<String> getPermissions(String roleName);
 }
--- trunk/src/main/java/com/catb/bo/impl/UserBOImpl.java
+++ trunk/src/main/java/com/catb/bo/impl/UserBOImpl.java
@@ -1,6 +1,8 @@
 package com.catb.bo.impl;
 
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -8,6 +10,8 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.catb.bo.UserBO;
 import com.catb.dao.UserDAO;
+import com.catb.model.Permission;
+import com.catb.model.Role;
 import com.catb.model.User;
 
 @Service
@@ -24,5 +28,45 @@ public class UserBOImpl implements UserBO {
 	@Transactional
 	public void addUser(User user) {
 		userDAO.addUser(user);
+	}
+	
+	@Transactional
+	public User getUserByUsername(String username) {
+		return userDAO.getUserByUsername(username);
+	}
+	
+	@Transactional
+	public Set<Role> getRolesForUser(String username) {
+		User user = getUserByUsername(username);
+		return user.getRoles();
+	}
+	
+	@Transactional
+	public Set<String> getRoleNamesForUser(String username) {
+		Set<Role> roles = getRolesForUser(username);
+		Set<String> roleNames = new LinkedHashSet<String>();
+		if (roles != null) {
+			for (Role role : roles) {
+				String roleName = role.getName();
+				if (roleName != null) {
+					roleNames.add(roleName);
+				}
+			}
+		}
+		
+		return roleNames;
+	}
+	
+	@Transactional
+	public Set<String> getPermissions(String roleName) {
+		List<Permission> permissions = userDAO.getPermissionsByRole(roleName);
+		Set<String> permissionSet = new LinkedHashSet<String>();
+		for (Permission p : permissions) {
+			if (p.getName() != null) {
+				permissionSet.add(p.getName());
+			}
+		}
+		
+		return permissionSet;
 	}
 }
