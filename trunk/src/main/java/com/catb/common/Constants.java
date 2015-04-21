82c20eb8d979e828c167053394387bfd6eaa5351
trananhtho207@gmail.com <trananhtho207@gmail.com@683bf793-15f8-c8c1-9392-6aebd92667f2>
2015-04-21 03:27:57 +0000

    add new context to serve images

--- trunk/src/main/java/com/catb/common/Constants.java
+++ trunk/src/main/java/com/catb/common/Constants.java
@@ -13,6 +13,7 @@ public class Constants {
 	public static String COMMONINFO_CONFIG_FILE;
 	public static String DISPLAY_LOCATION_CONFIG_FILE;
 	public static String NEWS_IMAGE_LOCATION;
+	public static String NEWS_IMAGE_PATH;
 	
 	public static void load() {
 		try {
@@ -21,6 +22,7 @@ public class Constants {
 			COMMONINFO_CONFIG_FILE = config.getString("COMMONINFO_CONFIG_FILE");
 			DISPLAY_LOCATION_CONFIG_FILE = config.getString("DISPLAY_LOCATION_CONFIG_FILE");
 			NEWS_IMAGE_LOCATION = config.getString("NEWS_IMAGE_LOCATION");
+			NEWS_IMAGE_PATH = config.getString("NEWS_IMAGE_PATH");
 		} catch (ConfigurationException ex) {
 			logger.error("Loading configuration failed: ", ex);
 		}
